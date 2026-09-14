7351e3ab06dc22778894bd3b6ec2a099d997baec
Seaic Mac Murchadha <seaicmacmurchadha@yahoo.com>
2026-09-14 23:00:28 +0100

    Rename GCNsNet module to gcnsnet

--- braindecode/models/__init__.py
+++ braindecode/models/__init__.py
@@ -31,7 +31,7 @@ from .emg2qwerty import EMG2QwertyNet
 from .fbcnet import FBCNet
 from .fblightconvnet import FBLightConvNet
 from .fbmsnet import FBMSNet
-from .gcns_net import GCNsNet
+from .gcnsnet import GCNsNet
 from .hybrid import HybridNet
 from .ifnet import IFNet
 from .interpolated import InterpolatedModel
